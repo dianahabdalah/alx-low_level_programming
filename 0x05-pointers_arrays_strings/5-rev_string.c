3cc550791a5c8434e0a7ec2c507a7e2d6245c039
dianahabdalah <dianahabdalah@gmail.com>
2023-03-27 04:44:57 -0700

    a good engenier

--- 0x05-pointers_arrays_strings/5-rev_string.c
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -1,4 +1,4 @@
-include "main.h"
+#include "main.h"
 /**
  * rev_string - Reverses a string
  * @s: Input string
@@ -9,7 +9,7 @@ void rev_string(char *s)
 char rev = s[0];
 int counter = 0;
 int i;
-while (s[counter] != '\0'
+while (s[counter] != '\0')
 counter++;
 for (i = 0; i < counter; i++)
 {
