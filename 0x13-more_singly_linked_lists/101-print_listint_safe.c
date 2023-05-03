0b4173c6e842aaa98933ba550031378a2595caed
dianahabdalah <dianahabdalah@gmail.com>
2023-05-03 07:40:20 -0700

    Print (safe version)

--- 0x13-more_singly_linked_lists/101-print_listint_safe.c
+++ 0x13-more_singly_linked_lists/101-print_listint_safe.c
@@ -1,42 +1,88 @@
 #include "lists.h"
 #include <stdio.h>
-#include <stdlib.h>
+
+size_t looped_listint_len(const listint_t *head);
+size_t print_listint_safe(const listint_t *head);
 
 /**
- * print_listint_safe - prints a listint_t linked list.
- * @head: pointer to the head of the list
+ * looped_listint_len - Counts the number of unique nodes
+ * in a looped listint_t linked list.
+ * @head: A pointer to the head of the listint_t to check.
  *
- * Return: the number of nodes in the list
+ * Return: If the list is not looped - 0.
+ * Otherwise - the number of unique nodes in the list.
+ */
+size_t looped_listint_len(const listint_t *head)
+{
+	const listint_t *ptr1, *ptr2;
+	size_t nodes = 1;
+
+	if (head == NULL || head->next == NULL)
+		return (0);
+
+	ptr1 = head->next;
+	ptr2 = (head->next)->next;
+
+	while (ptr2)
+	{
+		if (ptr1 == ptr2)
+		{
+			ptr1 = head;
+			while (ptr1 != ptr2)
+			{
+				nodes++;
+				ptr1 = ptr1->next;
+				ptr2 = ptr2->next;
+			}
+
+			ptr1 = ptr1->next;
+			while (ptr1 != ptr2)
+			{
+				nodes++;
+				ptr1 = ptr1->next;
+			}
+
+			return (nodes);
+		}
+
+		ptr1 = ptr1->next;
+		ptr2 = (ptr2->next)->next;
+	}
+
+	return (0);
+}
+
+/**
+ * print_listint_safe - Prints a listint_t list safely.
+ * @head: A pointer to the head of the listint_t list.
+ *
+ * Return: The number of nodes in the list.
  */
 size_t print_listint_safe(const listint_t *head)
 {
-	const listint_t *current = head, *loop_start = NULL;
-	size_t count = 0;
+	size_t nodes, index = 0;
 
-	while (current != NULL)
+	nodes = looped_listint_len(head);
+
+	if (nodes == 0)
 	{
-		if (current > current->next && loop_start == NULL)
-			loop_start = current;
-
-		printf("[%p] %d\n", (void *)current, current->n);
-		count++;
-		current = current->next;
-
-		if (current == loop_start)
+		for (; head != NULL; nodes++)
 		{
-			printf("Loop starts at [%p] %d\n", (void *)loop_start, loop_start->n);
-			break;
+			printf("[%p] %d\n", (void *)head, head->n);
+			head = head->next;
 		}
 	}
 
-	if (loop_start != NULL)
-		count += printf("-> [%p] %d\n", (void *)loop_start, loop_start->n);
-
-	if (count == 0)
+	else
 	{
-		fprintf(stderr, "Error: empty list\n");
-		exit(98);
+		for (index = 0; index < nodes; index++)
+		{
+			printf("[%p] %d\n", (void *)head, head->n);
+			head = head->next;
+		}
+
+		printf("-> [%p] %d\n", (void *)head, head->n);
 	}
 
-	return (count);
+	return (nodes);
 }
