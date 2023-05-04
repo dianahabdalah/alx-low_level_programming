3c4e929a912da18a92fbfccbc08b345aa9230618
dianahabdalah <dianahabdalah@gmail.com>
2023-05-04 06:08:08 -0700

    binary to unit

--- 0x14-bit_manipulatio/0-binary_to_uint.c
+++ 0x14-bit_manipulatio/0-binary_to_uint.c
@@ -1,45 +1,26 @@
-#include <stdio.h>
-#include <stdlib.h>
+#include "main.h"
 
 /**
- * binary_to_uint - converts a binary number to an unsigned int
- * @b: pointer to a string of 0 and 1 chars
+ * binary_to_uint - converts a binary number to unsigned int
+ * @b: string containing the binary number
  *
- * Return: the converted number, or 0 if there is one or more chars in the
- *         string b that is not 0 or 1, or if b is NULL.
+ * Return: the converted number
  */
 unsigned int binary_to_uint(const char *b)
 {
-	unsigned int result = 0;
 	int i;
+	unsigned int dec_val = 0;
 
-	if (b == NULL)
+	if (!b)
 		return (0);
 
-	for (i = 0; b[i] != '\0'; i++)
+	for (i = 0; b[i]; i++)
 	{
-		if (b[i] != '0' && b[i] != '1')
+		if (b[i] < '0' || b[i] > '1')
 			return (0);
-
-		result <<= 1;
-		result += b[i] - '0';
+		dec_val = 2 * dec_val + (b[i] - '0');
 	}
 
-	return (result);
+	return (dec_val);
 }
 
-/**
- * main - entry point
- *
- * Return: always 0 (success)
- */
-int main(void)
-{
-	char *binary = "10101010101010101010";
-	unsigned int decimal;
-
-	decimal = binary_to_uint(binary);
-	printf("The decimal equivalent of %s is %u\n", binary, decimal);
-
-	return (0);
-}
