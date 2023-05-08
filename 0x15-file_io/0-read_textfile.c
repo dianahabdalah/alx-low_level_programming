6104afb20a2d9065da1a552d1eefbb2107e0b6f6
dianahabdalah <dianahabdalah@gmail.com>
2023-05-08 05:34:15 -0700

    Tread lightly, she is near

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -4,26 +4,21 @@
 /**
  * read_textfile- Read text file print to STDOUT.
  * @filename: text file being read
- * @letters: number of letters to be read
- * Return: w- actual number of bytes read and printed
- *        0 when function fails or filename is NULL.
+ * @num_chars: number of characters to be read
+ * Return: num_written - actual number of bytes read and printed
+ *         0 when function fails or filename is NULL.
  */
-ssize_t read_textfile(const char *filename, size_t letters)
+ssize_t read_textfile(const char *filename, size_t num_chars)
 {
-	char *buf;
-	ssize_t xy;
-	ssize_t z;
-	ssize_t s;
-
-	fd = open(filename, O_RDONLY);
-	if (fd == -1)
-		return (0);
-	buf = malloc(sizeof(char) * letters);
-	s = read(xy, buf, letters);
-	z = write(STDOUT_FILENO, buf, s);
-
-	free(buf);
-	close(xy);
-	return (z);
+char *bf;
+ssize_t fd, rd, num_written;
+fd = open(filename, O_RDONLY);
+if (fd == -1)
+return (0);
+bf = malloc(sizeof(char) * num_chars);
+rd = read(fd, bf, num_chars);
+num_written = write(STDOUT_FILENO, bf, rd);
+free(bf);
+close(fd);
+return (num_written);
 }
-
